cbfaf16f81a94d62a47dec6b90e1d28866abea10
Krish Grover <gkrish088@gmail.com>
2026-05-23 18:19:46 +0000

    views classes using generic classs

--- snippets/views.py
+++ snippets/views.py
@@ -239,26 +239,42 @@ So:
 a = 1
 kwargs = {'name':'Krish','age':20}
 """
-class SnippetList (
-    mixins.ListModelMixin, mixins.CreateModelMixin , generics.GenericAPIView
-):
+# class SnippetList (
+#     mixins.ListModelMixin, mixins.CreateModelMixin , generics.GenericAPIView
+# ):
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
+
+#     def get(self , request , *args , **kwargs):
+#         return self.list(request , *args , **kwargs)
+#     def post(self , request , *args , **kwargs):
+#         return self.create(request , *args , **kwargs)
+
+# class SnippetDetails(
+#     mixins.RetrieveModelMixin, mixins.UpdateModelMixin , mixins.DestroyModelMixin , generics.GenericAPIView
+# ):
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
+
+#     def get(self , request , *args , **kwargs):
+#         return self.retrieve(request , *args , **kwargs)
+#     def put(self , request ,  *args , **kwargs):
+#         return self.update(request , *args , **kwargs)
+#     def delete(self , request , *args , **kwargs):
+#         return self.destroy(request , *args , **kwargs)
+
+
+
+"""
+yeh jo likha h yeh bkc h bc sala sab khud hi bana rakha h in kamino ne classes me , bs specify krna tha konse konse function uthane h 
+"""
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework import generics
+
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self , request , *args , **kwargs):
-        return self.list(request , *args , **kwargs)
-    def post(self , request , *args , **kwargs):
-        return self.create(request , *args , **kwargs)
-
-class SnippetDetails(
-    mixins.RetrieveModelMixin, mixins.UpdateModelMixin , mixins.DestroyModelMixin , generics.GenericAPIView
-):
+class SnippetDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self , request , *args , **kwargs):
-        return self.retrieve(request , *args , **kwargs)
-    def put(self , request ,  *args , **kwargs):
-        return self.update(request , *args , **kwargs)
-    def delete(self , request , *args , **kwargs):
-        return self.destroy(request , *args , **kwargs)
