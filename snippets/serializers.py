5a42903c847068487dc66b289d42bd1e9efb8967
Krish Grover <gkrish088@gmail.com>
2026-05-23 13:36:26 +0000

    making serializers using ModelSerializers class

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -17,49 +17,59 @@ Deletion normally happens in the view.
 from rest_framework import serializers
 from snippets.models import Snippet , LANGUAGE_CHOICES , STYLE_CHOICES
 
-class SnippetSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only = True)
-    title = serializers.CharField(required = False , allow_blank = True , max_length = 100)
-    code = serializers.CharField(style = {"base_template" : "textarea.html"}) # this tells when showing this feild in the DRF web interface , use a large multilinie text area instead of one line text input 
-    linenos = serializers.BooleanField(required = True)
-    language = serializers.ChoiceField(choices=LANGUAGE_CHOICES , default = "python") # here the choice feild is  there but in models it was char feild becaause , in db we only store strings and do not require much validation , but when taking taking an input via  api we need to validate is the input is among the choices or not .
-    style = serializers.ChoiceField(choices=STYLE_CHOICES , default = "friendly")
+# class SnippetSerializer(serializers.Serializer):
+#     id = serializers.IntegerField(read_only = True)
+#     title = serializers.CharField(required = False , allow_blank = True , max_length = 100)
+#     code = serializers.CharField(style = {"base_template" : "textarea.html"}) # this tells when showing this feild in the DRF web interface , use a large multilinie text area instead of one line text input 
+#     linenos = serializers.BooleanField(required = True)
+#     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES , default = "python") # here the choice feild is  there but in models it was char feild becaause , in db we only store strings and do not require much validation , but when taking taking an input via  api we need to validate is the input is among the choices or not .
+#     style = serializers.ChoiceField(choices=STYLE_CHOICES , default = "friendly")
 
-    def create(self , validated_data):
-        """
-        create and return a new Snippet instance 
+#     def create(self , validated_data):
+#         """
+#         create and return a new Snippet instance 
         
-        , given the validated data 
-        when data comes from the client in json format 
-        def receives a python dictionary , and we then do serializer = SnippetSerializer(data=data)
-        then serializer.is_valid() ,,this  is by default in the serializer class 
-        def validates everything , if successfull it passes the data to create(validated_data)
-        """
-        return Snippet.objects.create(**validated_data) #  '**' this is used to tell that the data is in dictioonary form and expand it into keyword arguments 
-        """ also the code we wrote above , it it shorter version of
-            def create(self, validated_data):
+#         , given the validated data 
+#         when data comes from the client in json format 
+#         def receives a python dictionary , and we then do serializer = SnippetSerializer(data=data)
+#         then serializer.is_valid() ,,this  is by default in the serializer class 
+#         def validates everything , if successfull it passes the data to create(validated_data)
+#         """
+#         return Snippet.objects.create(**validated_data) #  '**' this is used to tell that the data is in dictioonary form and expand it into keyword arguments 
+#         """ also the code we wrote above , it it shorter version of
+#             def create(self, validated_data):
 
-    snippet = Snippet(
-        title=validated_data["title"],
-        code=validated_data["code"],
-        language=validated_data["language"]
-    )
+#     snippet = Snippet(
+#         title=validated_data["title"],
+#         code=validated_data["code"],
+#         language=validated_data["language"]
+#     )
 
-    snippet.save()
+#     snippet.save()
 
-    return snippet
+#     return snippet
 
-    this code , and we can  see that .save() is automatically called above  
-        """ 
+#     this code , and we can  see that .save() is automatically called above  
+#         """ 
 
-    def update(self , instance , validated_data):
-        """
-        Update and return an existing `Snippet` instance, given the validated data.
-        """
-        instance.title = validated_data.get("title" , instance.title) # meaning = get this key if exists or use the default value  dictionary.get(key , default_value)
-        instance.code = validated_data.get("code" , instance.code)
-        instance.linenos = validated_data.get("linenos", instance.linenos)
-        instance.language = validated_data.get("language", instance.language)
-        instance.style = validated_data.get("style", instance.style)
-        instance.save()
-        return instance
+#     def update(self , instance , validated_data):
+#         """
+#         Update and return an existing `Snippet` instance, given the validated data.
+#         """
+#         instance.title = validated_data.get("title" , instance.title) # meaning = get this key if exists or use the default value  dictionary.get(key , default_value)
+#         instance.code = validated_data.get("code" , instance.code)
+#         instance.linenos = validated_data.get("linenos", instance.linenos)
+#         instance.language = validated_data.get("language", instance.language)
+#         instance.style = validated_data.get("style", instance.style)
+#         instance.save()
+#         return instance
+
+
+# the upper method is the django method where we only use Serializer class but rest framework gives a better option to use ModelSerializer class that inherits from serializer class
+# ModelSerializer class automatically creates the serializers by looking at the model and also generated the create() and update() methods . 
+
+class SnippetSerializers (serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ["id" , "title" , "code" , "linenos" , "language" , "style"]
+        
